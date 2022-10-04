0125c3c8376885db77cec8f2afd4c544b65695c1
michhkh <michaelh.kodehode@gmail.com>
2022-10-04 11:05:18 +0200

    Removed unnecessary code

--- src/components/styles/StyledHeader.js
+++ src/components/styles/StyledHeader.js
@@ -2,16 +2,6 @@ import styled from "styled-components";
 import { Link as LinkRouter } from "react-router-dom";
 import { Link as LinkScroll } from "react-scroll";
 
-// export const StyledNavContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   min-height: 5vh;
-//   font-size: 2rem;
-//   padding: 0.5em 6em 0.5em 8em;
-//   border: 1px solid #013a63;
-// `;
-
 export const StyledNavContainer = styled.div`
   width: 85%;
   margin: 0 auto;
@@ -30,18 +20,6 @@ export const StyledNavLogo = styled.div`
 `;
 
 export const StyledNav = styled.nav`
-  //   position: fixed;
-  //   background-color: var(--clr-dark);
-  //   color: var(--clr-light);
-  //   top: 0;
-  //   bottom: 0;
-  //   left: 0;
-  //   right: 0;
-  //   z-index: 100;
-
-  //   transform: translateX(0%);
-  //   transition: transform 250ms cubic-bezier(0.5, 0, 0.5, 1);
-
   @media screen and (min-width: 600px) {
     position: relative;
     display: flex;
@@ -97,20 +75,16 @@ export const StyledLink = styled(LinkScroll)`
 export const StyledUl = styled.ul`
   list-style: ${(props) => props.showNavs && "none"};
   display: ${(props) => props.showNavs && "none"};
-  list-style-position: ${(props) => props.aboutList && "inside"};
-  width: ${(props) => props.aboutList && "90%"};
-  margin: ${(props) => (props.aboutList ? "1em auto 0 auto" : "0")};
+  margin: 0;
   line-height: 1.6;
 
   @media (min-width: 600px) {
     display: ${(props) => props.showNavs && "flex"};
-    width: ${(props) => props.aboutList && "88%"};
-
     align-items: center;
     font-size: 1.4rem;
   }
 `;
 
 export const StyledLi = styled.li`
-  padding-top: ${(props) => (props.aboutList ? "1em" : "0")};
+  padding-top: 0;
 `;
